07e191a227c0772230adab1143006e4a165dda35
Andrei O. (andrei0x309) <andrei@flashsoft.eu>
2021-05-28 16:30:21 +0300

    intital commit

--- index.js
+++ index.js
@@ -2,24 +2,55 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const EosApi = require('eosjs-api');
+
+let isRam = true;
+// Enough for at least one transaction
+const mimimumRamRequired = 800;
+
+// Main EOS
+const rcpOptions = {
+  httpEndpoint: 'https://eos.greymass.com:443', // default, null for cold-storage
+  verbose: false, // API logging
+  logger: {
+    // Default logging functions
+    log: console.log,
+    error: console.error
+  },
+  fetchConfiguration: {},
+};
+
+const eosApi = EosApi(rcpOptions);
+
+const checkRam = async (reportChannel) => {
+  const yupAccountInfo = await eosApi.getAccount('yupyupyupyup');
+  console.log(yupAccountInfo.ram_quota - yupAccountInfo.ram_usage);
+
+  if ((yupAccountInfo.ram_quota - yupAccountInfo.ram_usage) < mimimumRamRequired) {
+    if (isRam) {
+      reportChannel.send('YUP RAM has exhausted, voting transactions will not work.');
+    }
+    isRam = false;
+  } else {
+    if (!isRam) {
+      reportChannel.send('YUP now has RAM, voting transactions will work.');
+    }
+    isRam = true;
+  }
+
+}
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
-});
-
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+  // 45 sec check so I wont get banned by GrayMass and waste CPU on bot hosting account
+  const ramChannel = bot.channels.find("name", "eos-ram-check")
+  if (ramChannel) {
+    const intervalObj = setInterval(() => {
+      checkRam(ramChannel);
+    }, 45000);
   }
 });
+
+
